f767094d7fa584672b9c3aae5ccb18c3499e3ff3
Leon Chisum <leonchisum3306@gmail.com>
2020-02-13 19:26:06 -0500

    finished building delete and put routes

--- index.js
+++ index.js
@@ -20,12 +20,78 @@ server.post("/api/users", (req, res) => {
 
        db.insert(newUser)
         .then(u => res.status(201).json(u))
-        .catch(err => res.status(404).json({error: `${err}`}))
+        .catch(err => res.status(500).json({errorMessage: "There was an error while saving the user to the database"}))
 
    } else {
        res.status(400).json({ errorMessage: "Please provide name and bio for user" })
    }
    
+
+})
+
+server.get("/api/users", (req, res) => {
+
+    db.find()
+        .then(u => res.json(u))
+        .catch(err => res.status(500).json({errorMessage: "The users information could not be retrieved."}))
+})
+
+server.get("/api/users/:id", (req, res) => {
+
+    const id = req.params.id
+    
+    if ( id != undefined ) {
+        
+        db.findById(id)
+            .then(u => res.status(200).json(u))
+            .catch(err => res.status(500).json({ errorMessage: "The user information could not be retrieved." }))
+
+    } else {
+
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+
+    }
+
+})
+
+server.delete("/api/users/:id", (req, res) => {
+
+    const id = req.params.id
+    
+    if (id) {
+        
+        db.remove(id)
+            .then(u => res.json(u))
+            .catch(err => res.status(500).json({ errorMessage: "The user information could not be retrieved." }))
+
+    } else {
+
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+
+    }
+
+})
+
+server.put("/api/users/:id", (req, res) => {
+
+    const { name, bio } = req.body
+    const id = req.params.id
+
+    
+    if (id == undefined) {
+        
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+
+    } else if (name == '' || bio == '') {
+
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user."})
+
+    } else {
+
+        db.update(id, req.body)
+            .then(u => res.status(200).json(u))
+            .catch(err => res.status(500).json({ errorMessage: "The user information could not be retrieved." }))
+    }
 
 })
 
